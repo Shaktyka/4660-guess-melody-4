5990c7c351a58b599450f0a108ba0fe80a37c62b
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-06-16 23:38:52 +0500

    Добавляет простой dev-роутинг

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import GameArtist from '../game-artist/game-artist.jsx';
+import GameGenre from '../game-genre/game-genre.jsx';
 
 const welcomeButtonClickHandler = () => {};
 
@@ -8,10 +12,22 @@ const App = (props) => {
   const {errorsAmount} = props;
 
   return (
-    <WelcomeScreen
-      errors={errorsAmount}
-      welcomeButtonClickHandler={welcomeButtonClickHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errors={errorsAmount}
+            welcomeButtonClickHandler={welcomeButtonClickHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <GameArtist />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GameGenre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
