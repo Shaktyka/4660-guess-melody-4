dc5eadb13597daecc1b1fead29ad67fc0e3922ad
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-06-27 19:27:43 +0500

    Добавляет функц-ный компонент GameMistakes

---
+++ src/components/game-mistakes/game-mistakes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GameMistakes = () => {
+  return (
+    <div className="game__mistakes">
+      <div className="wrong"></div>
+      <div className="wrong"></div>
+      <div className="wrong"></div>
+    </div>
+  );
+};
+
+export default GameMistakes;
